5d12b93e9462e92bf69298e3b138fb419d0c1fd4
José <eltitosetto@gmail.com>
2024-05-28 20:14:25 +0200

    pagination, footer & productlist
    - added pagination
    - added footer
    - modified productlist to work with pagination

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react'; 
 import productService from '../services/productService';
 import ProductCard from './ProductCard';
+import Pagination from './pagination'; // Asegúrate de importar el componente Pagination
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1); // Estado para almacenar la página actual
+  const [totalPages, setTotalPages] = useState(1); // Estado para almacenar el total de páginas
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const data = await productService.getProducts();
+        const data = await productService.getProducts({}, currentPage); // Pasa currentPage al servicio
         setProducts(data.products);
+        setTotalPages(data.totalPages); // Actualiza el total de páginas
         setLoading(false);
       } catch (error) {
         setError(error.message);
@@ -20,7 +24,11 @@ const ProductList = () => {
     };
 
     fetchProducts();
-  }, []);
+  }, [currentPage]); // Ejecuta el efecto cuando currentPage cambia
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page); // Actualiza la página actual cuando se cambia de página
+  };
 
   return (
     <div className="container mx-auto p-4">
@@ -30,11 +38,14 @@ const ProductList = () => {
       ) : error ? (
         <p>Error: {error}</p>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
+        <>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+          <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} /> {/* Pasa las propiedades al componente Pagination */}
+        </>
       )}
     </div>
   );
