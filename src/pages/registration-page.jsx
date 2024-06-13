21e3b79ccaa7cf91ae9de4a55ed68346cfc45d8e
José <eltitosetto@gmail.com>
2024-06-13 22:25:37 +0200

    few fixes

--- src/pages/registration-page.jsx
+++ src/pages/registration-page.jsx
@@ -2,9 +2,11 @@ import RegistrationForm from '../components/RegistrationForm';
 
 const RegistrationPage = () => {
     return (
-        <div className="container mx-auto py-8 px-8">
-            <h1 className="text-2xl font-semibold text-gray-800 mb-4">Registro de Usuario</h1>
-            <RegistrationForm />
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
+                <h1 className="text-2xl font-semibold text-gray-800 mb-4 text-center">Registro de Usuario</h1>
+                <RegistrationForm />
+            </div>
         </div>
     );
 };
