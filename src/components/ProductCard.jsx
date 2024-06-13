342e8b516c03f2aebd2099fe5509cdc6503353c5
José <eltitosetto@gmail.com>
2024-06-13 23:11:41 +0200

    product cart button conditions

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import PropTypes from 'prop-types'; // Importa PropTypes para la validación de propiedades
-import { ProductPropTypes } from '../utils/ProductPropTypes';
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import userService from '../services/userService';
 
 const Modal = ({ message, onClose }) => {
@@ -10,12 +9,12 @@ const Modal = ({ message, onClose }) => {
       <div className="relative bg-white p-4 rounded-lg shadow-lg">
         <button
           className="absolute top-0 right-2 bg-transparent text-gray-600 hover:text-gray-900 text-2xl"
-          style={{ fontSize: '2rem', lineHeight: '1' }} // Ajuste para hacer la "X" más grande y ajustar el espaciado
+          style={{ fontSize: '2rem', lineHeight: '1' }}
           onClick={onClose}
         >
           &times;
         </button>
-        <p className="mt-4 mb-4 ml-4 mr-10">{message}</p> {/* Añadir margen superior al texto */}
+        <p className="mt-4 mb-4 ml-4 mr-10">{message}</p>
       </div>
     </div>
   );
@@ -27,32 +26,43 @@ Modal.propTypes = {
 };
 
 const ProductCard = ({ product }) => {
-  const [addingToCart, setAddingToCart] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
   const [modalMessage, setModalMessage] = useState('');
+  const [authenticated, setAuthenticated] = useState(false); // Initialize as false
 
-  const getFormattedImageURL = (image) => {
-    const nameWithoutExtension = image.split('.')[0];
-    const folderName = nameWithoutExtension.slice(0, -2);
-    return `/products/${folderName}/${image}`;
-  };
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        await userService.getUserProfile();
+        setAuthenticated(true); // Update authenticated state if successful
+      } catch (error) {
+        console.error('User not authenticated:', error);
+        setAuthenticated(false); // Ensure authenticated is false on error
+      }
+    };
+
+    checkAuthentication();
+  }, []);
 
   const handleAddToCart = async () => {
+    if (!authenticated) {
+      setModalMessage('Debes iniciar sesión para añadir al carrito.');
+      setModalVisible(true);
+      return;
+    }
+
     try {
-      setAddingToCart(true);
       const response = await userService.addItemToCart(product._id);
       if (response.status === 'already_in_cart') {
-        setModalMessage('Este producto ya está en el carrito.');
+        setModalMessage('Este producto ya está en tu carrito.');
       } else {
-        setModalMessage('Producto añadido al carrito');
+        setModalMessage('Producto añadido al carrito correctamente.');
       }
       setModalVisible(true);
     } catch (error) {
       console.error('Error adding item to cart:', error);
-      setModalMessage('Este producto ya está en el carrito.'); //('Error al añadir el producto al carrito');
+      setModalMessage('Error al añadir el producto al carrito.');
       setModalVisible(true);
-    } finally {
-      setAddingToCart(false);
     }
   };
 
@@ -60,10 +70,20 @@ const ProductCard = ({ product }) => {
     setModalVisible(false);
   };
 
+  const getFormattedImageURL = (image) => {
+    const nameWithoutExtension = image.split('.')[0];
+    const folderName = nameWithoutExtension.slice(0, -2);
+    return `/products/${folderName}/${image}`;
+  };
+
   return (
     <div className="rounded-lg p-4 flex flex-col items-start justify-between bg-gray-800">
       <Link to={`/products/${product._id}`} className="link">
-        <img src={getFormattedImageURL(product.images[0])} alt={product.name} className="w-full h-full object-contain" />
+        <img
+          src={getFormattedImageURL(product.images[0])}
+          alt={product.name}
+          className="w-full h-full object-contain"
+        />
       </Link>
       <h3 className="text-xl font-bold mt-2 text-white">{product.name}</h3>
       <p className="text-white">{product.totalPrice} €</p>
@@ -77,11 +97,10 @@ const ProductCard = ({ product }) => {
         <span className="ml-2 text-white">{product.reviews} reseñas</span>
       </div>
       <button
-        className={`bg-blue-500 text-white px-4 py-2 mt-2 rounded ${addingToCart ? 'opacity-50 pointer-events-none' : 'hover:bg-blue-600'}`}
+        className="bg-blue-500 text-white px-4 py-2 mt-2 rounded hover:bg-blue-600"
         onClick={handleAddToCart}
-        disabled={addingToCart}
       >
-        {addingToCart ? 'Añadiendo al carrito...' : 'Añadir al carrito'}
+        Añadir al carrito
       </button>
       {modalVisible && <Modal message={modalMessage} onClose={closeModal} />}
     </div>
@@ -89,7 +108,14 @@ const ProductCard = ({ product }) => {
 };
 
 ProductCard.propTypes = {
-  product: ProductPropTypes.isRequired
+  product: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    totalPrice: PropTypes.number.isRequired,
+    averageRating: PropTypes.number.isRequired,
+    reviews: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default ProductCard;
