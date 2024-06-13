71957d44c18cad7b623b744686924c7c51021d16
José <eltitosetto@gmail.com>
2024-06-13 18:23:01 +0200

    profile better view

--- src/pages/profile-page.jsx
+++ src/pages/profile-page.jsx
@@ -13,12 +13,18 @@ const ProfilePage = () => {
     }
 
     return (
-        <div className="max-w-7xl mx-auto p-6 bg-white shadow-lg rounded-lg">
-            <h1 className="text-2xl font-semibold text-gray-800 mb-4">Perfil</h1>
-            <UserProfile />
-            <OrderHistory />
-        </div>
-    );
+      <div className='container mx-auto py-8 px-8'>
+          <div className="max-w-7xl mx-auto p-6 bg-white shadow-lg rounded-lg mb-4">
+              <h1 className="text-2xl font-semibold text-gray-800 mb-4">Perfil</h1>
+              <UserProfile />
+          </div>
+          <div className="max-w-7xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+              <h2 className="text-2xl font-semibold text-gray-800 mb-4">Historial de Pedidos</h2>
+              <OrderHistory />
+          </div>
+      </div>
+  );
+  
 };
 
 export default ProfilePage;
